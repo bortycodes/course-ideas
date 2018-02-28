36133f75a556ab1957b3199d4beca0ac0a768261
gp borla <gpborla>
2018-02-28 23:40:09 +0800

    added post method for sign-in

--- src/main/java/com/teamgp/courses/Main.java
+++ src/main/java/com/teamgp/courses/Main.java
@@ -1,6 +1,10 @@
 package com.teamgp.courses;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -11,6 +15,14 @@ public class Main {
 		
 		get("/", (req, res) -> {
 			return new ModelAndView(null, "index.hbs");
+		}, new HandlebarsTemplateEngine());
+		
+		post("/sign-in", (req, res) -> {
+			Map<String, String> model = new HashMap<>();
+			model.put("username", req.queryParams("username"));
+			
+			
+			return new ModelAndView(model, "sign-in.hbs");
 		}, new HandlebarsTemplateEngine());
 	}
 }
